dda1bd39fb0e4e5379f3cff0d17e35fa36c14ace
Jairo0911 <jairo.lagos@unah.hn>
2022-08-12 18:34:44 -0600

    Creacion de el .html y .js, para la entidad Docente

---
+++ JS/Docente.js
@@ -0,0 +1,5 @@
+var UrlDocentes = 'http://20.216.41.245:90/G1_19/controller/docente.php?op=GetDocentes';
+var UrlInsertDocente = 'http://20.216.41.245:90/G1_19/controller/docente.php?op=InsertDocente';
+var UrlGetDocente = 'http://20.216.41.245:90/G1_19/controller/docente.php?op=GetDocente';
+var UrlUpdateDocente = 'http://20.216.41.245:90/G1_19/controller/estudiante.php?op=UpdateEstudiante';
+var UrlDeleteDocente = 'http://20.216.41.245:90/G1_19/controller/estudiante.php?op=DeleteEstudiante';
